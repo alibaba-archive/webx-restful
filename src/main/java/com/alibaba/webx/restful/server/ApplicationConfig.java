34b4d281513d62f664f278008bcc28e23b28f4d9
温少 <szujobs@hotmail.com>
2012-06-12 10:58:24 +0800

    init project

--- src/main/java/com/alibaba/webx/restful/server/ApplicationConfig.java
+++ src/main/java/com/alibaba/webx/restful/server/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package com.alibaba.webx.restful.server;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.core.Application;
 
@@ -30,6 +33,61 @@ public class ApplicationConfig extends Application {
 
     public Set<ResourceFinder> getResourceFinders() {
         return resourceFinders;
+    }
+
+    /**
+     * Get a canonical array of String elements from a String array where each entry may contain zero or more elements
+     * separated by ';'.
+     * 
+     * @param elements an array where each String entry may contain zero or more ';' separated elements.
+     * @return the array of elements, each element is trimmed, the array will not contain any empty or null entries.
+     */
+    public static String[] getElements(String[] elements) {
+        // keeping backwards compatibility
+        return getElements(elements, ";");
+    }
+
+    /**
+     * Get a canonical array of String elements from a String array where each entry may contain zero or more elements
+     * separated by characters in delimiters string.
+     * 
+     * @param elements an array where each String entry may contain zero or more delimiters separated elements.
+     * @param delimiters string with delimiters, every character represents one delimiter.
+     * @return the array of elements, each element is trimmed, the array will not contain any empty or null entries.
+     */
+    public static String[] getElements(String[] elements, String delimiters) {
+        List<String> es = new LinkedList<String>();
+        for (String element : elements) {
+            if (element == null) continue;
+            element = element.trim();
+            if (element.length() == 0) continue;
+            for (String subElement : getElements(element, delimiters)) {
+                if (subElement == null || subElement.length() == 0) continue;
+                es.add(subElement);
+            }
+        }
+        return es.toArray(new String[es.size()]);
+    }
+
+    /**
+     * Get a canonical array of String elements from a String that may contain zero or more elements separated by
+     * characters in delimiters string.
+     * 
+     * @param elements a String that may contain zero or more delimiters separated elements.
+     * @param delimiters string with delimiters, every character represents one delimiter.
+     * @return the array of elements, each element is trimmed.
+     */
+    private static String[] getElements(String elements, String delimiters) {
+        String regex = "[";
+        for (char c : delimiters.toCharArray())
+            regex += Pattern.quote(String.valueOf(c));
+        regex += "]";
+
+        String[] es = elements.split(regex);
+        for (int i = 0; i < es.length; i++) {
+            es[i] = es[i].trim();
+        }
+        return es;
     }
 
 }
---
+++ src/main/java/com/alibaba/webx/restful/server/internal/scanning/UriSchemeResourceFinderFactory.java
@@ -0,0 +1,19 @@
+package com.alibaba.webx.restful.server.internal.scanning;
+
+import java.net.URI;
+import java.util.Set;
+
+import com.alibaba.webx.restful.server.ResourceFinder;
+
+
+interface UriSchemeResourceFinderFactory {
+
+    /**
+     * Get the set of supported URI schemes.
+     * 
+     * @return the supported URI schemes.
+     */
+    Set<String> getSchemes();
+
+    ResourceFinder create(URI u);
+}
