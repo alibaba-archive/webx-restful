ce9d114d7a391ee4c55efc8412de1d2ca7d0df32
温少 <szujobs@hotmail.com>
2012-06-20 10:10:53 +0800

    add type converter

--- src/main/java/com/alibaba/webx/restful/server/process/converter/BigIntegerConverter.java
+++ src/main/java/com/alibaba/webx/restful/server/process/converter/BigIntegerConverter.java
@@ -3,7 +3,7 @@ package com.alibaba.webx.restful.server.process.converter;
 import java.math.BigInteger;
 
 
-public class BIgIntegerConverter implements TypeConverter {
+public class BigIntegerConverter implements TypeConverter {
 
     @Override
     public Object convert(String literalValue) {
