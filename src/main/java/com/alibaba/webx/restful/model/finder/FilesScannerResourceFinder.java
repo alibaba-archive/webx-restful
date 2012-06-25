452a55aaf5cd66a96a7951e6fe4abcaf3a2fe347
温少 <szujobs@hotmail.com>
2012-06-25 09:08:20 +0800

    refactor

---
+++ src/main/java/com/alibaba/webx/restful/model/finder/FilesScannerResourceFinder.java
@@ -0,0 +1,76 @@
+package com.alibaba.webx.restful.model.finder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+final class FilesScannerResourceFinder implements ResourceFinder {
+
+    private final FilesScanner filesScanner;
+    private final File         f;
+    Stack<File>                files;
+    private File               current;
+    private File               next;
+
+    FilesScannerResourceFinder(FilesScanner filesScanner, final File f){
+        this.filesScanner = filesScanner;
+        this.f = f;
+        files = new Stack<File>() {
+
+            {
+                if (f.isDirectory()) {
+                    for (File file : f.listFiles()) {
+                        push(file);
+                    }
+                } else {
+                    push(f);
+                }
+            }
+        };
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (next == null && !files.empty()) {
+            next = files.pop();
+
+            if (next.isDirectory() || next.getName().endsWith(".jar") || next.getName().endsWith(".zip")) {
+                this.filesScanner.processFile(next);
+                next = null;
+            }
+        }
+
+        return next != null;
+    }
+
+    @Override
+    public String next() {
+        if (next != null || hasNext()) {
+            current = next;
+            next = null;
+            return current.getName();
+        }
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public InputStream open() {
+        try {
+            return new FileInputStream(current);
+        } catch (FileNotFoundException e) {
+            throw new ResourceFinderException(e);
+        }
+    }
+
+    @Override
+    public void reset() {
+    }
+}
